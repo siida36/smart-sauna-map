9b77619f6b30bbc1fbef8892e709353982bedc4c
Taisei Watanabe <taisei_watanabe@albert2005.co.jp>
2022-03-09 10:15:01 +0900

    Introduce layout

--- front/smart-sauna-map/src/components/App.js
+++ front/smart-sauna-map/src/components/App.js
@@ -1,63 +1,84 @@
 import React, { useState } from 'react';
+import {
+  Grid, ThemeProvider, CssBaseline,
+} from '@mui/material';
+
+import theme from '../theme/theme';
 import './App.css';
 import Maps from './Map';
 import QueryForm from './QueryForm';
-
+import LongMenu from './LongMenu';
 
 function App() {
-  const [center, setCenter] = useState({lat: 35.683542, lng: 139.703338});
+  const [center, setCenter] = useState({ lat: 35.683542, lng: 139.703338 });
   const [saunas, setSaunas] = useState([]);
-  const [query, setQuery] = useState("");
+  const [query, setQuery] = useState('');
 
   const handleFormInput = (event) => {
     // state でも property でもなく event.target.value で値を渡す
     setQuery(event.target.value);
-  }
+  };
 
-  const handleFormSubmit= (event) => {
-    event.preventDefault();  // デフォルトではページのリロードが行われる。これを防ぐため。
+  const handleFormSubmit = (event) => {
+    event.preventDefault(); // デフォルトではページのリロードが行われる。これを防ぐため。
     updateMapViewCenter(setCenter, query);
-    updateSaunas(setSaunas, query, "");
-  }
+    updateSaunas(setSaunas, query, '');
+  };
 
   return (
-    <div className="App">
-      <QueryForm
-        query={ query }
-        onInput={ handleFormInput }
-        onSubmit={ handleFormSubmit } />
-      <Maps center={ center } saunas={ saunas } />
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div className="App">
+        <Grid container spacing={3} justifyContent="center" alignItems="center">
+          <Grid item xs={10}>
+            <p>SmartSaunaMap</p>
+          </Grid>
+          <Grid item xs={2}>
+            <LongMenu />
+          </Grid>
+        </Grid>
+        <Grid container spacing={3} justifyContent="center" alignItems="center">
+          <Grid item xs={12}>
+            <QueryForm
+              query={query}
+              onInput={handleFormInput}
+              onSubmit={handleFormSubmit}
+            />
+          </Grid>
+        </Grid>
+        <Maps center={center} saunas={saunas} />
+      </div>
+    </ThemeProvider>
   );
 }
 
-function updateMapViewCenter(setCenter, query){
-  const url = "http://localhost:5000/";
-  createPromise(url, {"query": query})
-    .then(location => {
-      setCenter({lat: location.lat, lng: location.lng});
+function updateMapViewCenter(setCenter, query) {
+  const url = 'http://localhost:5000/';
+  createPromise(url, { query })
+    .then((location) => {
+      setCenter({ lat: location.lat, lng: location.lng });
     });
 }
 
-function updateSaunas(setSaunas, keyword="shinjuku", prefecture="") {
+function updateSaunas(setSaunas, keyword = 'shinjuku', prefecture = '') {
   // Python のサウナイキタイパースサーバにクエリを投げてサウナ情報を取得する関数.
-  const url = "http://localhost:5000/sauna";
-  createPromise(url, {"keyword": keyword, "prefecture": prefecture})
-    .then(saunas => {
+  const url = 'http://localhost:5000/sauna';
+  createPromise(url, { keyword, prefecture })
+    .then((saunas) => {
       setSaunas(saunas);
     });
 }
 
-async function createPromise(url, query){
-  const requestOptions ={
-      method: 'POST',
-      headers:{'Content-Type': 'application/json'},
-      body: JSON.stringify(query)
+async function createPromise(url, query) {
+  const requestOptions = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(query),
   };
 
-  return await fetch(url, requestOptions)
-    .then(response => response.json())
-    .catch(error => console.error(error));
+  return fetch(url, requestOptions)
+    .then((response) => response.json())
+    .catch((error) => { throw new Error(error); });
 }
 
 export default App;
--- server/src/smart_sauna_map/geocoding.py
+++ server/src/smart_sauna_map/geocoding.py
@@ -3,6 +3,7 @@
 from __future__ import annotations
 import os
 from os.path import join, dirname
+from socket import timeout
 import geopy
 from dotenv import load_dotenv
 
@@ -11,9 +12,9 @@ load_dotenv(dotenv_path=join(dirname(__file__), ".env"))
 GOOGLE_MAP_API_KEY = os.environ.get("GOOGLE_MAP_API_KEY")
 
 
-def geocode(query: str) -> dict[str, float | None]:
+def geocode(query: str, timeout: float = 30.0) -> dict[str, float | None]:
     location = geopy.geocoders.GoogleV3(
-        api_key=GOOGLE_MAP_API_KEY, domain="maps.google.co.jp"
+        api_key=GOOGLE_MAP_API_KEY, domain="maps.google.co.jp", timeout=timeout
     ).geocode(query)
     if location:
         return {"lat": location.latitude, "lng": location.longitude}
